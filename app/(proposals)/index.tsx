e098e30d52179d61981da9df8001188ac731908a
Damir Vazgird <damir@withpulp.com>
2024-02-24 21:45:45 -0800

    e2e update, more code chores

--- app/(proposals)/index.tsx
+++ app/(proposals)/index.tsx
@@ -1,5 +1,3 @@
-import { View } from 'react-native';
-
 import Link from '@/components/Link';
 import Screen from '@/components/Screen';
 
@@ -18,19 +16,24 @@ const proposals = [
   },
 ];
 
-const ProposalsList = () => (
-  <View className="w-full items-center justify-center gap-2">
+// Can't use this right now - list won't render in ios simulator after refreshing web app
+// const ProposalsList = () => (
+//   <View className="w-full items-center justify-center gap-2">
+//     {proposals.map(({ id, title }) => (
+//       <Link key={id} href={`/${id}`} className="w-full text-center text-xl">
+//         {title}
+//       </Link>
+//     ))}
+//   </View>
+// );
+
+const ProposalsView = () => (
+  <Screen className="items-center justify-center">
     {proposals.map(({ id, title }) => (
       <Link key={id} href={`/${id}`} className="w-full text-center text-xl">
         {title}
       </Link>
     ))}
-  </View>
-);
-
-const ProposalsView = () => (
-  <Screen className="items-center justify-center">
-    <ProposalsList />
   </Screen>
 );
 
